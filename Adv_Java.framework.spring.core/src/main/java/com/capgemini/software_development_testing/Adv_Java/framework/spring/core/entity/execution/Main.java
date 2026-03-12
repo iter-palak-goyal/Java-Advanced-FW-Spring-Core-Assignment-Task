6234afa460050a8dc6c0bd88b3ea2f59636b22a9
Palak Goyal <palak2@Apples-MacBook-Air.local>
2026-03-12 23:03:59 +0530

    Class work 12 march 2026

---
+++ Adv_Java.framework.spring.core/src/main/java/com/capgemini/software_development_testing/Adv_Java/framework/spring/core/entity/execution/Main.java
@@ -0,0 +1,10 @@
+package com.capgemini.software_development_testing.Adv_Java.framework.spring.core.entity.execution;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaApp.execution();
+	}
+
+}
